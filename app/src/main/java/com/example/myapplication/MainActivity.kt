f853d66c74d5f0c26bb1c9b5fca32631adf64bb5
Nikites-dev <petrovnx@gmail.com>
2023-09-14 16:28:13 +0300

    refactor code

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,13 +1,14 @@
 package com.example.myapplication
 
+import android.R.attr.button
 import android.annotation.SuppressLint
 import android.content.Intent
 import android.graphics.Bitmap
 import android.graphics.Canvas
-import android.graphics.Color
 import android.os.Bundle
 import android.os.Environment
 import android.view.View
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.FileProvider
 import com.example.myapplication.databinding.ActivityMainBinding
@@ -26,151 +27,40 @@ class MainActivity : AppCompatActivity() {
         val viewRoot = binding.root
         setContentView(viewRoot)
 
-
-//        val img: ImageView = binding.linearLayout as ImageView
-
-
-
-//        // Get the view or layout you want to convert
-//        val view = findViewById<View>(R.id.linearLayout)
-//
-//        // Create a bitmap and draw the view on it
-////        val bitmap = Bitmap.createBitmap(100, 60, Bitmap.Config.ARGB_8888)
-////        val canvas = Canvas(bitmap)
-////        view.draw(canvas)
-//
-//
-//        val bitmap = Bitmap.createBitmap(100, 100, Bitmap.Config.ARGB_8888)
-//
-//        // Create a new Canvas object using the Bitmap
-//
-//        // Create a new Canvas object using the Bitmap
-//        val canvas = Canvas(bitmap)
-//
-//        // Draw the View into the Canvas
-//
-//        // Draw the View into the Canvas
-//        view.draw(canvas)
-//
-//
-//
-//        binding.image.setImageBitmap(bitmap);
-//
-
-
-//        val filename = "image.png"
-//        val file = File(getExternalFilesDir(Environment.DIRECTORY_PICTURES), filename)
-//
-//        // Save the bitmap to the file
-//        var outputStream: FileOutputStream? = null
-//        try {
-//            outputStream = FileOutputStream(file)
-//            if (bitmap != null) {
-//                bitmap.compress(Bitmap.CompressFormat.PNG, 100, outputStream)
-//            }
-//            outputStream.flush()
-//        } catch (e: Exception) {
-//            e.printStackTrace()
-//        } finally {
-//            outputStream?.close()
-//        }
-//
-//
-//
-//
-//
-//
-//        // Create an intent with appropriate action and type
-//        val intent = Intent(Intent.ACTION_SEND)
-//        intent.type = "image/*"
-//
-//        // Attach the image file to the intent
-//        val uri = FileProvider.getUriForFile(this, "com.example.myapplication", file)
-//        intent.putExtra(Intent.EXTRA_STREAM, uri)
-//
-//        // Add any text or subject if needed
-//        intent.putExtra(Intent.EXTRA_TEXT, "Sharing an image")
-//        intent.putExtra(Intent.EXTRA_SUBJECT, "Shared Image")
-//
-//        // Grant permission to the receiver app to read the shared file
-//        intent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION)
-//
-//        // Start the intent
-//        startActivity(Intent.createChooser(intent, "Share Image"))
-//
-//
-
-
-
-
+//        binding.button.setOnClickListener(View.OnClickListener {
+//            Toast.makeText(this, "Click!!!", Toast.LENGTH_SHORT).show()
+//          //  shareBitmapFile(file)
+//        })
     }
 
-    fun getBitmapFromView(view: View): Bitmap? {
-        //Define a bitmap with the same size as the view
-        val returnedBitmap = Bitmap.createBitmap(1000, 1000, Bitmap.Config.ARGB_8888)
-        //Bind a canvas to it
-        val canvas = Canvas(returnedBitmap)
-        //Get the view's background
-        val bgDrawable = view.background
-        if (bgDrawable != null) //has background drawable, then draw it on the canvas
-            bgDrawable.draw(canvas) else  //does not have background drawable, then draw white background on the canvas
-            canvas.drawColor(Color.WHITE)
-        // draw the view on the canvas
-        view.draw(canvas)
-        //return the bitmap
-        return returnedBitmap
-    }
 
-    private fun getBitmapFromViewUsingCanvas(view: View): Bitmap? {
-        // Create a new Bitmap object with the desired width and height
-        val bitmap = Bitmap.createBitmap(view.width, view.height, Bitmap.Config.ARGB_8888)
-
-        // Create a new Canvas object using the Bitmap
-        val canvas = Canvas(bitmap)
-
-        // Draw the View into the Canvas
-        view.draw(canvas)
-
-        // Return the resulting Bitmap
-        return bitmap
-    }
 
     override fun onWindowFocusChanged(hasFocus: Boolean) {
         super.onWindowFocusChanged(hasFocus)
-        val view = findViewById<View>(R.id.linearLayout)
+        val view = binding.linearLayout
 
-        // Create a bitmap and draw the view on it
-//        val bitmap = Bitmap.createBitmap(100, 60, Bitmap.Config.ARGB_8888)
-//        val canvas = Canvas(bitmap)
-//        view.draw(canvas)
-
-
-        val bitmap = Bitmap.createBitmap(view.width, view.height, Bitmap.Config.ARGB_8888)
-
-        // Create a new Canvas object using the Bitmap
-
-        // Create a new Canvas object using the Bitmap
-        val canvas = Canvas(bitmap)
-
-        // Draw the View into the Canvas
-
-        // Draw the View into the Canvas
-        view.draw(canvas)
-
+        val bitmap = getBitmapFromView(view)
         binding.image.setImageBitmap(bitmap);
 
-
-
-
-
-
-
-
-
-                val filename = "image2.png"
+        val filename = "image.png"
         val file = File(getExternalFilesDir(Environment.DIRECTORY_PICTURES), filename)
+        saveBitmapFile(bitmap, file)
 
-        // Save the bitmap to the file
+        binding.textView.setOnClickListener(View.OnClickListener {
+            Toast.makeText(this, "Click!!!", Toast.LENGTH_SHORT).show()
+            shareBitmapFile(file)
+        })
+    }
+
+    fun getBitmapFromView(view: View): Bitmap? {
+        val bitmap = Bitmap.createBitmap(view.width, view.height, Bitmap.Config.ARGB_8888)
+        val canvas = Canvas(bitmap)
+        view.draw(canvas)
+
+        return bitmap
+    }
+
+    fun saveBitmapFile(bitmap: Bitmap?, file: File) {
         var outputStream: FileOutputStream? = null
         try {
             outputStream = FileOutputStream(file)
@@ -183,12 +73,9 @@ class MainActivity : AppCompatActivity() {
         } finally {
             outputStream?.close()
         }
+    }
 
-
-
-
-
-
+    fun shareBitmapFile(file: File) {
         // Create an intent with appropriate action and type
         val intent = Intent(Intent.ACTION_SEND)
         intent.type = "image/*"
@@ -198,17 +85,12 @@ class MainActivity : AppCompatActivity() {
         intent.putExtra(Intent.EXTRA_STREAM, uri)
 
         // Add any text or subject if needed
-        intent.putExtra(Intent.EXTRA_TEXT, "Sharing an image")
-        intent.putExtra(Intent.EXTRA_SUBJECT, "Shared Image")
+//        intent.putExtra(Intent.EXTRA_TEXT, "Sharing Image")
+//        intent.putExtra(Intent.EXTRA_SUBJECT, "Shared Image")
 
-        // Grant permission to the receiver app to read the shared file
         intent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION)
 
         // Start the intent
         startActivity(Intent.createChooser(intent, "Share Image"))
-
-
-
     }
-
 }
